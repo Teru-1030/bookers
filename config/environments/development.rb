7ba5c08e23f6fa8caf1758d7f4592ba57f6627d1
Teru-1030 <166922869+Teru-1030@users.noreply.github.com>
2024-05-13 11:49:50 +0000

    [add] データベース上に必要なテーブル

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "ad4b5dc01d8c4dc3816c1452f8f1f9a5.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
