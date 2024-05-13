679a3f841291f3b1da461548b71f65da4b3c8554
Teru-1030 <166922869+Teru-1030@users.noreply.github.com>
2024-05-13 15:29:03 +0000

    [add] ルートディレクトリへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
+  get 'new' => 'books#top'
+  get 'create' => 'books#create'
+  get 'index' => 'books#index'
+  get 'show' => 'books#show'
+  get 'edit' => 'books#edit'
+  get 'update' => 'books#update'
+  get 'destroy' => 'books#destro'
   get 'homes/top'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
