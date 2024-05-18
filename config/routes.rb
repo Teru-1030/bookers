1e527edb4d4aaea6d4a5dcac2b3ff2a6afc15a96
Teru-1030 <166922869+Teru-1030@users.noreply.github.com>
2024-05-18 09:50:55 +0000

    [add]削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  get 'books' => 'books#new'
-  post 'books' => 'books#create'
-  get  'books/:id' =>'books#show', as: 'book'
-  get 'books/:id/edit' =>'books#edit', as: 'edit_book'
-  patch 'update' => 'books#update'
-  get 'destroy' => 'books#destroy'
   get 'homes/top'
+  resources :books
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
